bdf5a482d5970011453761556919df0e26a374a1
Alex McNeill <alex.mcneill@gmail.com>
2019-06-06 10:11:25 +0100

    Add screen

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = () => {
   return (
     <div className="App">
       <article className="calculator">
+        <div className="screen">1234</div>
         <p>Calculator</p>
       </article>
     </div>
