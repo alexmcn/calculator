1c937d737570a2c0da2737ad10a4db767bac1add
Alex McNeill <alex.mcneill@gmail.com>
2019-06-06 10:16:27 +0100

    Add branding

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ const App = () => {
     <div className="App">
       <article className="calculator">
         <div className="screen">1234</div>
+        <div className="brand">
+          <img
+            alt="Equal Experts"
+            src="https://www.equalexperts.com/wp-content/themes/equalexperts/assets/logo.svg"
+          />
+        </div>
         <p>Calculator</p>
       </article>
     </div>
