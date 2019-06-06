4cdc897c2cf7dee6193aef111f1d9efd7161ed33
Alex McNeill <alex.mcneill@gmail.com>
2019-06-06 10:41:05 +0100

    Add number pad

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.scss';
 
 const App = () => {
+  const numberPadOrder = [10, 7, 8, 9, 4, 5, 6, 1, 2, 3];
+
   return (
     <div className="App">
       <article className="calculator">
@@ -12,7 +14,18 @@ const App = () => {
             src="https://www.equalexperts.com/wp-content/themes/equalexperts/assets/logo.svg"
           />
         </div>
-        <p>Calculator</p>
+
+        <div className="pad">
+          <div className="numberKeys">
+            {numberPadOrder.map((order, index) => (
+              <button key={index} style={{ order }}>
+                {index}
+              </button>
+            ))}
+            <button style={{ order: 11 }}>.</button>
+          </div>
+          <div className="operatorKeys">/ x - =</div>
+        </div>
       </article>
     </div>
   );
